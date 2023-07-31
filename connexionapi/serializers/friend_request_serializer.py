d324746964a626310e3c4bfe3e84b4181b81c347
Vision Filler <visionfiller@gmail.com>
2023-07-31 13:43:10 -0500

    friend requests

--- connexionapi/serializers/friend_request_serializer.py
+++ connexionapi/serializers/friend_request_serializer.py
@@ -1,9 +1,10 @@
 from rest_framework import serializers
 from connexionapi.models import FriendRequest
-
+from .connexion_friend_serializer import ConnexionFriendSerializer
 
 class FriendRequestSerializer(serializers.ModelSerializer):
     """Serializes the swapper model to convert it to useable json"""
+    connexion_user_from = ConnexionFriendSerializer(many=False)
     class Meta:
         model = FriendRequest
         fields = ('id', 'connexion_user_to', 'connexion_user_from', 'status')
